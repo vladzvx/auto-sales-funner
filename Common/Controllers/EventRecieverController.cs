9b5b359c7176c4c1329bf0ec619fa944b1f86ea4
vladzvx <wladzwer@mail.ru>
2021-11-26 02:07:03 +0300

    test:

--- Common/Controllers/EventRecieverController.cs
+++ Common/Controllers/EventRecieverController.cs
@@ -28,44 +28,50 @@ namespace Common.Controllers
         [HttpPost("recieve")]
         public async Task<string> RecieveEvent(CancellationToken token)
         {
-            GetLead getLead = new GetLead();
-            DealUpdate du = new DealUpdate(); 
-            MoveLead ml = new MoveLead(); 
-            CreateContact cc = new CreateContact(); 
-            DealCreator dc = new DealCreator(); 
-            using (StreamReader reader = new StreamReader(this.Request.Body))
+            respones = "";
+            foreach (var v in Request.Form.Keys)
             {
-                try
-                {
-                    paramts = this.Request.ContentType;
-                    string data = await reader.ReadToEndAsync();
-                    respones = data;
-                    getLead.IdLead = "";
-                    await Utils.Requests.ExecuteGet(getLead.Create, null, async (str) =>
-                    {
-                        using (var cont = dbContextFactory.CreateDbContext())
-                        {
-                            var res = await cont.Contacts.Where(item => item.Phone == "2222").ToListAsync();
-                            if (res.Any())
-                            {
-                                await Utils.Requests.ExecuteGet(du.Create, null, null);
-                                await Utils.Requests.ExecuteGet(ml.Create, null, null);
-                                //что записать в БД?
-                            }
-                            else
-                            {
-                                await Utils.Requests.ExecuteGet(cc.Create, null, null);
-                                await Utils.Requests.ExecuteGet(dc.Create, null, null);
-                            }
-                        }
-                    });
-                }
-                catch (Exception ex)
-                {
-                    paramts = Newtonsoft.Json.JsonConvert.SerializeObject(ex);
-                }
-
+                respones += v;
             }
+             
+            //GetLead getLead = new GetLead();
+            //DealUpdate du = new DealUpdate(); 
+            //MoveLead ml = new MoveLead(); 
+            //CreateContact cc = new CreateContact(); 
+            //DealCreator dc = new DealCreator(); 
+            //using (StreamReader reader = new StreamReader(this.Request.Body))
+            //{
+            //    try
+            //    {
+            //        paramts = this.Request.ContentType;
+            //        string data = await reader.ReadToEndAsync();
+            //        respones = data;
+            //        getLead.IdLead = "";
+            //        await Utils.Requests.ExecuteGet(getLead.Create, null, async (str) =>
+            //        {
+            //            using (var cont = dbContextFactory.CreateDbContext())
+            //            {
+            //                var res = await cont.Contacts.Where(item => item.Phone == "2222").ToListAsync();
+            //                if (res.Any())
+            //                {
+            //                    await Utils.Requests.ExecuteGet(du.Create, null, null);
+            //                    await Utils.Requests.ExecuteGet(ml.Create, null, null);
+            //                    //что записать в БД?
+            //                }
+            //                else
+            //                {
+            //                    await Utils.Requests.ExecuteGet(cc.Create, null, null);
+            //                    await Utils.Requests.ExecuteGet(dc.Create, null, null);
+            //                }
+            //            }
+            //        });
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        paramts = Newtonsoft.Json.JsonConvert.SerializeObject(ex);
+            //    }
+
+            //}
             return "ok";
         }
 
