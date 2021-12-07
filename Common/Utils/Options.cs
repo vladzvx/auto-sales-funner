4b3ab3f8c13b01d40813879608394673be11c926
Владислав Звержховский <wladzwer@mail.ru>
2021-12-07 14:33:52 +0300

    options.upd

--- Common/Utils/Options.cs
+++ Common/Utils/Options.cs
@@ -10,8 +10,8 @@ namespace Common.Utils
     {
         public static string Target { get; } = Environment.GetEnvironmentVariable("API_KEY");
         public static string EntityTypeId { get; } = Environment.GetEnvironmentVariable("ENTITY_TYPE_ID") ?? "162";
-        public static string StageId { get; } = Environment.GetEnvironmentVariable("NEW_STAGE_ID") ?? "DT162_2:NEW";
-        public static string StageId2 { get; } = Environment.GetEnvironmentVariable("STAGE_ID2") ?? "DT162_2:UC_Y6K9JP";
+        public static string StageId { get; } = Environment.GetEnvironmentVariable("NEW_STAGE_ID");
+        public static string StageId2 { get; } = Environment.GetEnvironmentVariable("STAGE_ID2");
         public static string CategoryId { get; } = Environment.GetEnvironmentVariable("CATEGORY_ID") ?? "2.0";
         public static string Domain { get; } = Environment.GetEnvironmentVariable("DOMAIN");
     }
