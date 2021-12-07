28e64f3bb1ceb8e1cf90dafe510cb47d8c5b30db
Владислав Звержховский <wladzwer@mail.ru>
2021-12-07 12:51:08 +0300

    remove launch setting

--- Common/Utils/Options.cs
+++ Common/Utils/Options.cs
@@ -8,10 +8,10 @@ namespace Common.Utils
 {
     public static class Options
     {
-        public static string Target { get; } = Environment.GetEnvironmentVariable("Target") ?? "12/ck6uqtx0ne2pm1xj";
-        public static string EntityTypeId { get; } = Environment.GetEnvironmentVariable("EntityTypeId") ?? "162";
-        public static string StageId { get; } = Environment.GetEnvironmentVariable("StageId") ?? "DT162_2:NEW";
-        public static string CategoryId { get; } = Environment.GetEnvironmentVariable("CategoryId") ?? "2.0";
-        public static string Domain { get; } = Environment.GetEnvironmentVariable("Domain") ?? "l.osbb-kr.com.ua";
+        public static string Target { get; } = Environment.GetEnvironmentVariable("API_KEY");
+        public static string EntityTypeId { get; } = Environment.GetEnvironmentVariable("ENTITY_TYPE_ID") ?? "162";
+        public static string StageId { get; } = Environment.GetEnvironmentVariable("NEW_STAGE_ID") ?? "DT162_2:NEW";
+        public static string CategoryId { get; } = Environment.GetEnvironmentVariable("CATEGORY_ID") ?? "2.0";
+        public static string Domain { get; } = Environment.GetEnvironmentVariable("DOMAIN") ?? "l.osbb-kr.com.ua";
     }
 }
